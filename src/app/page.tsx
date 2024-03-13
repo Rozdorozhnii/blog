cb766b69495b3522bb6af33ea9dbee2c0d0d14ab
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-03-13 20:19:04 +0200

    clear template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,7 @@
-import styles from './page.module.css';
-import course from './course.json';
-import { Lesson } from './lesson';
-
-import dynamic from 'next/dynamic';
-const ClientComponent = dynamic(() => import('./some-component'), {
-  ssr: false,
-});
-
-async function getAllCourses(): Promise<typeof course> {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(course), 1000);
-  });
-}
-
 export default async function Home() {
-  const courses = await getAllCourses();
-
   return (
-    <main className={styles.main}>
-      <ul>
-        <h2>React</h2>
-        <ClientComponent />
-        {course.lessons.map(lesson => (
-          <li key={lesson.name}>
-            <Lesson title={lesson.title} shortSummary={lesson.shortSummary} />
-          </li>
-        ))}
-      </ul>
+    <main>
+      <h2>React</h2>
     </main>
   );
 }
--- src/app/some-component.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-export default function SomeComponent() {
-  console.log('Some component rendered');
-  return <p>Some component rendered on client only</p>;
-}
